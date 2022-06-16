9f4a72553b52e343b891460959f9f63ff75d19f8
sam <wyysammore@gmail.com>
2022-06-16 16:43:19 +0800

    feat: defineProperty archive

---
+++ demo/index.ts
@@ -0,0 +1,25 @@
+import EventEmitter from '../utils/event-emitter';
+
+const em = new EventEmitter();
+
+const store = { a: 1, b: 1 };
+
+// autorun
+const fn = () => console.log(store.a);
+
+// observable
+Object.defineProperty(store, 'a', {
+  get: function() {
+    em.on('store.a', fn);
+    return 100;
+  },
+  set: function() {
+    em.emit('store.a');
+  },
+});
+
+// collect dependencies
+fn();
+
+// set state
+store.a = 2;
