27f57deabbba6902ca450478971154c7021ec8a2
sam <wyysammore@gmail.com>
2022-07-11 10:49:21 +0800

    fix: change file constructor

--- demo/index.ts
+++ demo/index.ts
@@ -1,25 +1,9 @@
-import EventEmitter from '../utils/event-emitter';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const em = new EventEmitter();
+const App = () => {
+  return <h1 title='foo'>Hello</h1>;
+};
 
-const store = { a: 1, b: 1 };
-
-// autorun
-const fn = () => console.log(store.a);
-
-// observable
-Object.defineProperty(store, 'a', {
-  get: function() {
-    em.on('store.a', fn);
-    return 100
-  },
-  set: function() {
-    em.emit('store.a');
-  },
-});
-
-// collect dependencies
-fn();
-
-// set state
-store.a = 2;
+const container = document.getElementById('root');
+ReactDOM.render(<App />, container);
